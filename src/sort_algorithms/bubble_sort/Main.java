4d23a3ff9fb20a483e5e228ab29a8f51d6123700
Euller_Gabriel <eullergoliveira@gmail.com>
2025-01-13 19:54:43 +0000

    Added: Moved BubbleSort code explanation to a different file to keep code readability

--- src/sort_algorithms/bubble_sort/Main.java
+++ src/sort_algorithms/bubble_sort/Main.java
@@ -9,21 +9,8 @@ public class Main {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
         System.out.println(Arrays.toString(intArray));
 
-        /*
-        * Outer Loop that splits the array on unsorted/sorted regions by keeping track of the last unsorted index,
-        * starting at the last index of the array. At each iteration the lastUnsortedIndex will be decremented to
-        * indicate the new lastUnsortedIndex.
-        * The previous index stored in the lastUnsortedIndex will be occupied by the sorted value
-        */
-        for (
-                int lastUnsortedIndex = intArray.length - 1;
-                lastUnsortedIndex > 0;
-                lastUnsortedIndex--
-        ) {
-            /*
-            * Inner loop traverses the array until current lastUnsortedIndex, swapping values when necessary at each
-            * iteration.
-            */
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
             for (int i = 0; i < lastUnsortedIndex; i++) {
                 if (intArray[i] > intArray[i + 1]) {
                     swap(intArray, i, i + 1);
@@ -32,6 +19,9 @@ public class Main {
         }
         System.out.println(Arrays.toString(intArray));
     }
+
+
+
 
     // Swaps the position of 2 values on a given array based on their index.
     public static void swap(int[] array, int i, int j){
@@ -43,11 +33,5 @@ public class Main {
         array[i] = array[j]; // Changes the value at index i to the value contained in index j
         array[j] = temp;     // Changes the value at index j to the value that war previously contained at index i
     }
-
-    /*
-    * Bubble sort is also a Stable Sort algorithm, which means when comparing 2 equal elements, these elements positions
-    * are not changed. This happens because of the if block in the swap method, that checks when elements are equal to
-    * each other, and when they are the code stops and goes for the next iteration.
-    */
 
 }
