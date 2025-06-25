3799ce6d5e907b44d31592da15618bf1b35b2ba4
Euller_Gabriel <eullergoliveira@gmail.com>
2025-06-25 12:32:10 +0100

    Improved ArrayQueue class to function as a circular queue

--- src/queue/ArrayQueue.java
+++ src/queue/ArrayQueue.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.NoSuchElementException;
 
 //Generic class implementation
+//Circular Queue
 public class ArrayQueue<T> {
     private T[] queue;
     private int front;
@@ -16,14 +17,27 @@ public class ArrayQueue<T> {
 
     @SuppressWarnings("unchecked")
     public void add(T object){
-        if (back == queue.length) {
+        if (size() == queue.length - 1) { // Resizing only happens if there are no empty positions in the array.
+            int numItems = size();
             T[]  newArray = (T[]) new Object[2 * queue.length];
-            System.arraycopy(queue,0,newArray,0,queue.length);
+
+            //Unwrap the queue
+            System.arraycopy(queue,front,newArray,0,queue.length - front);
+            System.arraycopy(queue,0,newArray,queue.length - front,back);
+
+            front = 0;
+            back = numItems;
             queue = newArray;
         }
 
         queue[back]=object;
-        back++;
+        // Implementation for circular queue
+        if (back < queue.length - 1) { // If there is still space in the back positions of the array, the back pointer will be incremented
+            back++;
+        }
+        else{ // If the back of the array is full, the back pointer will point to the empty front positions
+            back = 0;
+        }
     }
 
     public T remove(){
@@ -36,6 +50,8 @@ public class ArrayQueue<T> {
         if (size() == 0) {
             front = 0;
             back = 0;
+        } else if (front == queue.length) {
+            front = 0;
         }
         return object;
     }
@@ -49,12 +65,27 @@ public class ArrayQueue<T> {
     }
 
     public int size(){
-        return back - front;
+        if (front <= back) {
+            return back - front;
+        }
+        else{
+            return back - front + queue.length;
+        }
     }
 
     public void printQueue(){
-        for (int i = front; i < back; i++) {
-            System.out.println(queue[i]);
+        if (front <= back) {
+            for (int i = front; i < back; i++) {
+                System.out.println(queue[i]);
+            }
+        }
+        else{
+            for (int i = front; i < queue.length; i++) {
+                System.out.println(queue[i]);
+            }
+            for (int i = 0; i < back ; i++) {
+                System.out.println(queue[i]);
+            }
         }
     }
 }
